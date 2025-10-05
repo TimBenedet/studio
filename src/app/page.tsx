cccfb6111f3995a2da27160ff75c98553fa5be54
timothée bénédet (Ezzmazz) <timothee.benedet@protonmail.com>
2025-10-05 18:39:00 +0000

    Es tu sûr de ce que tu as fait ? L'affichage sera le même, peu importe l

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import { AccessForm } from "@/app/components/access-form";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-8">
+    <main className="flex min-h-screen flex-col items-center justify-center p-8">
       <div className="w-full flex-1 flex items-center justify-center">
         <AccessForm />
       </div>
